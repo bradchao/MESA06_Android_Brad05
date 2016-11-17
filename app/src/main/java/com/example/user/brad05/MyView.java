b10cd5ac6c6fe19e023d8a2e76669929a73cd917
Brad Chao <brad@brad.tw>
2016-11-17 10:23:13 +0800

    v4

--- app/src/main/java/com/example/user/brad05/MyView.java
+++ app/src/main/java/com/example/user/brad05/MyView.java
@@ -1,7 +1,9 @@
 package com.example.user.brad05;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,5 +11,13 @@ public class MyView extends View {
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(100,100,40, paint);
     }
 }
