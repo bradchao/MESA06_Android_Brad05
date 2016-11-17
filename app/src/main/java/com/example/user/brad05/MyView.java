41318ee3a7c869ea4048a2c63730b834ae6aa793
Brad Chao <brad@brad.tw>
2016-11-17 10:38:21 +0800

    v5

--- app/src/main/java/com/example/user/brad05/MyView.java
+++ app/src/main/java/com/example/user/brad05/MyView.java
@@ -5,12 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MyView extends View {
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.BLACK);
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("brad", "onClick");
+            }
+        });
     }
 
     @Override
@@ -19,5 +28,11 @@ public class MyView extends View {
         Paint paint = new Paint();
         paint.setColor(Color.YELLOW);
         canvas.drawCircle(100,100,40, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.v("brad", "onTouchEvent");
+        return super.onTouchEvent(event);
     }
 }
